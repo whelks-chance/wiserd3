7ffbcb802e212933c1751050991a1c59ac6bd0a3
ianh <iharvey111@gmail.com>
2015-07-08 00:05:27 +0100

    Initial commit

---
+++ dataportal3/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.template import RequestContext
+
+
+def index(request):
+    return render(request, 'index.html', {}, context_instance=RequestContext(request))
+
+
+def blank(request):
+    return render(request, 'blank.html', {}, context_instance=RequestContext(request))
+
+
+def tables(request):
+    return render(request, 'tables.html', {}, context_instance=RequestContext(request))

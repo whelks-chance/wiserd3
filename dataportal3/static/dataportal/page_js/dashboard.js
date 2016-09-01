b06ee388b279958f497540ca5192f6e5c2ea8620
Katie <a@b.c>
2016-09-01 15:31:12 +0100

    Tips added & css changed

--- dataportal3/static/dataportal/page_js/dashboard.js
+++ dataportal3/static/dataportal/page_js/dashboard.js
@@ -3,19 +3,41 @@ function do_intro() {
     bootstro.start('', {
         onStep: function(obj) {
             // {#  alert(' --- ' + obj.idx + ' --- ' + obj.direction);#}
-
+        if (obj.idx == 2) {
+            if (!$('#Recent_Search_Results_li').hasClass('active')) {
+                $('#Recent_Search_Results_btn').click();
+            }
+        }
         },
         items: [
             {
                 selector: '#lang_toggle_split',
                 title: 'Change language',
-                content: 'These buttons can be used to toggle the language the site displays in.' +
+                content: 'These buttons can be used throughout the website to toggle the language the site displays in.' +
                 '<br><br>' +
                 'If you sign-up and login, this preference will be stored for future visits.',
                 placement: 'bottom',
                 step: 0
+            },
+
+            {
+                selector: '#dashboard_grid',
+                title: 'The Dashboard',
+                content: 'These buttons take you to different parts of the DataPortal',
+                placement: 'left',
+                step: 1
+            },
+
+            {
+                selector: '#nav_bar_side',
+                title: 'Side Menu',
+                content: 'Content',
+                placement: 'right',
+                step: 2
+
             }
 
         ]
     });
 }
+

c72e162ba7d631a2e30dd375a455e6daa6449bd0
Katie <dicksonc5@cardiff.ac.uk>
2016-09-27 12:24:09 +0100

    tip boxes added to browse surveys

---
+++ dataportal3/static/dataportal/page_js/browse_surveys.js
@@ -0,0 +1,35 @@
+
+function do_intro() {
+    bootstro.start('', {
+        prevButton: '',
+        onStep: function(obj) {
+            // alert(' --- ' + obj.idx + ' --- ' + obj.direction);
+
+        },
+        items: [
+            {
+                selector: '#survey_table',
+                title: 'Browse Surveys',
+                content: 'This page allows you to browse all the surveys available.',
+                placement: 'top',
+                step: 0
+            },
+            {
+                selector: '#survey_table_filter',
+                title: 'Filter',
+                content: 'You can filter the table of surveys using key words',
+                placement: 'left',
+                step: 1
+            },
+            {
+                selector: '#survey_table_length',
+                title: 'View',
+                content: 'This button allows you to choose the number of surveys you view per page.',
+                placement: 'right',
+                step: 2
+            }
+        ]
+    });
+
+
+}

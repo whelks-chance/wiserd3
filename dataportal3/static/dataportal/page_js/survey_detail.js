c655b09823ea60d801e77d42ee9b4911ac3a3055
ianh <i.c.harvey@cs.cf.ac.uk>
2017-01-11 13:32:23 +0000

    v3.7.0.8 wordcloud in survey detail - still do not use

--- dataportal3/static/dataportal/page_js/survey_detail.js
+++ dataportal3/static/dataportal/page_js/survey_detail.js
@@ -6,7 +6,7 @@
 function do_intro() {
     bootstro.start('', {
         prevButton: '',
-        onStep: function(obj) {
+        onStep: function (obj) {
             // alert(' --- ' + obj.idx + ' --- ' + obj.direction);
 
 
@@ -15,7 +15,7 @@ function do_intro() {
                 // $('#survey_dc_tab').tab('show');
 
             }
-             if (obj.idx == 3 || obj.idx == 4) {
+            if (obj.idx == 3 || obj.idx == 4) {
                 $('#survey_tab_li').click();
                 // $('#survey_tab').tab('show');
 
@@ -80,6 +80,95 @@ function do_intro() {
         ]
     });
 
+}
+
+function wordCloud(selector) {
+    var fill = d3.scale.category20();
+    var svg = d3.select(selector).append("svg")
+        .attr("width", 600)
+        .attr("height", 500)
+        .append("g")
+        .attr("transform", "translate(250,250)");
+    function draw(words) {
+        var cloud = svg.selectAll("g text")
+            .data(words, function(d) { return d.text; });
+        //Entering words
+        cloud.enter()
+            .append("text")
+            .style("font-family", "Impact")
+            .style("fill", function(d, i) { return fill(i); })
+            .attr("text-anchor", "middle")
+            .attr('font-size', 1)
+            .text(function(d) { return d.text; });
+        cloud
+            .transition()
+            .duration(600)
+            .style("font-size", function(d) { return d.size + "px"; })
+            .attr("transform", function(d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .style("fill-opacity", 1);
+        cloud.exit()
+            .transition()
+            .duration(200)
+            .style('fill-opacity', 1e-6)
+            .attr('font-size', 1)
+            .remove();
+    }
+    return {
+        update: function(words) {
+            d3.layout.cloud().size([500, 500])
+                .words(words)
+                .padding(5)
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .font("Impact")
+                .fontSize(function(d) { return d.size; })
+                .on("end", draw)
+                .start();
+        }
+    }
+}
+
+
+function do_wordcloud(element_name, survey_api_url) {
+
+    var json = (function()
+    {
+        $.ajax({
+            'type': 'GET',
+            'async': false,
+            'global': false,
+            'url': survey_api_url,
+            'dataType': 'json',
+            'success' : function (data) {
+                json = data['results'][0]['keywords'];
+            }
+        });
+        return [json];
+    });
+
+    var words = ["test data goes here from different file at some point"
+    ];
+
+    console.log(words);
+    words = json();
+    console.log(words);
+
+    function getWords(i) {
+        return words[i]
+            .replace(/[!\.,:;\?]/g, '')
+            .split(' ')
+            .map(function(d) {
+                return {text: d, size: 10 + Math.random() * 60};
+            })
+    }
+    function showNewWords(vis, i) {
+        i = i || 0;
+        vis.update(getWords(i ++ % words.length));
+        setTimeout(function() { showNewWords(vis, i + 1)}, 2000)
+    }
+    var myWordCloud = wordCloud(element_name);
+    showNewWords(myWordCloud);
 
 }
 

2bf07487e3fa98f9b30b0a0c8bb2f8e8c5045163
Katie <dicksonc5@cardiff.ac.uk>
2019-12-11 14:38:57 +0000

    survey update

--- dataportal3/management/commands/ImportXLSX.py
+++ dataportal3/management/commands/ImportXLSX.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
             sr = SurveyReader(filename)
             sr.build_from_description_file(
                 description_file,
-                '4',
-                'C'
+                '1',
+                'B'
             )
 

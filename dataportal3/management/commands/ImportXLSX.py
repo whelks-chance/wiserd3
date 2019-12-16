cd16e8425999efcb80fd9d7f98e47b67b3e95fb5
Katie <dicksonc5@cardiff.ac.uk>
2019-12-16 10:04:21 +0000

    testing to add variableid to questions

--- dataportal3/management/commands/ImportXLSX.py
+++ dataportal3/management/commands/ImportXLSX.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
             sr = SurveyReader(filename)
             sr.build_from_description_file(
                 description_file,
-                '5',
-                'E'
+                '6',
+                'X'
             )
 
